0df7625ba29f7019ff791e43b2ce032be242f95d
Sam Yao <43796777+yaohuangguan@users.noreply.github.com>
2026-09-22 10:38:33 +1200

    test: cover tools and multiple project categories

--- tests/projectCategory.test.js
+++ tests/projectCategory.test.js
@@ -7,8 +7,18 @@ describe('Project categories', () => {
     expect(project.validateSync()).toBeUndefined();
   });
 
-  test.each(['web', 'fullstack', 'mobile'])('accepts %s projects', (category) => {
+  test.each(['web', 'fullstack', 'mobile', 'tools'])('accepts %s projects', (category) => {
     const project = new Project({ title_zh: '项目', title_en: 'Project', category });
+    expect(project.validateSync()).toBeUndefined();
+  });
+
+  test('accepts multiple categories', () => {
+    const project = new Project({
+      title_zh: '项目',
+      title_en: 'Project',
+      categories: ['tools', 'web']
+    });
+    expect(project.categories).toEqual(['tools', 'web']);
     expect(project.validateSync()).toBeUndefined();
   });
 
